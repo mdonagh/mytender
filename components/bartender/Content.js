9271fa4af7d7686e9d0fe67a753ef8946fa3fa1b
Mark P. Donahue <mark@record360.com>
2023-01-16 09:56:29 -0800

    remove unused

--- components/bartender/Content.js
+++ components/bartender/Content.js
@@ -1,24 +1,9 @@
-import { View,
-         Text,
-         Image,
-         StyleSheet,
-         Dimensions} from "react-native";
 import Description from './Description.js'
 import Rideshare from './RideShare.js'
 import Payment from './Payment.js'
 
 function Content(props) {
-  const {width, height} = Dimensions.get('window');
   const selected = props.selected;
-
-  const styles = StyleSheet.create({
-    image: {
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'contain'
-    },
-  });
 
   if (selected == 'rideshare') {
     return <Rideshare />;
