d5ce09a0b1188708a1f7007560dd4e108b9f1899
Mark P. Donahue <mark@record360.com>
2023-01-16 07:51:49 -0800

    navbar on show bartender page

--- components/ShowBartender.js
+++ components/ShowBartender.js
@@ -8,9 +8,18 @@ function ShowBartender() {
 
   const {width, height} = Dimensions.get('window');
 
+  const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'contain'
+    },
+  });
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', }}>
-      <Image style={{ flex: 2,
+      <Image style={{ flex: 6,
                       alignItems: 'center',
                       justifyContent: 'center',
                       borderBottomColor: 'black',
@@ -18,7 +27,26 @@ function ShowBartender() {
                       width: width }}
         source={{uri: 'https://i1.wp.com/www.curiositymag.com/wp-content/uploads/2019/03/Maria-Varamo_Curiosity-1.jpeg'}}
       />
-
+      <View style={{ flex: 1,
+                     alignItems: 'center',
+                     justifyContent: 'flex-start',
+                     flexDirection: 'row',
+                     flexWrap: 'wrap',
+                     padding: 10
+                    }}>
+        <View style={{flex: 1}}>
+          <Image source={require('../assets/question-mark.png')} style={styles.image} />
+        </View>
+        <View style={{flex: 1}}>
+          <Image source={require('../assets/money.png')} style={styles.image}  />
+        </View>
+        <View style={{flex: 1}}>
+          <Image source={require('../assets/full-heart.png')} style={styles.image}  />
+        </View>
+        <View style={{flex: 1}}>
+          <Image source={require('../assets/taxi.png')} style={styles.image}  />
+        </View>
+      </View>
       <Text style={{ flex: 1,
                       alignItems: 'center',
                       justifyContent: 'center',
@@ -26,11 +54,12 @@ function ShowBartender() {
                       borderBottomWidth: StyleSheet.hairlineWidth, 
                       fontSize: 20,
                       fontWeight: 'bold',
+                      padding: 20
                     }}
       >
         Bridget
       </Text>
-      <Text style={{ flex: 1,
+      <Text style={{ flex: 3,
                       alignItems: 'center',
                       justifyContent: 'center',
                       borderBottomColor: 'black',
@@ -41,7 +70,7 @@ function ShowBartender() {
       >
         Noble Experiment
       </Text>
-      <Text style={{ flex: 1,
+      <Text style={{ flex: 3,
                       alignItems: 'center',
                       justifyContent: 'center',
                       borderBottomColor: 'black',
