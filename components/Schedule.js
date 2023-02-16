bf5e2bd0c90b4bad0128e73ac07747ee77f8fc35
Mark P. Donahue <mark@record360.com>
2023-02-15 19:32:19 -0800

    add buttons

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,16 +1,49 @@
+import React from 'react';
+
 import { 
          Pressable,
-         Text,
+         View,
        } from "react-native";
+import { Button, ButtonGroup, withTheme, Text } from '@rneui/themed';
 
 import { ListItem, Avatar } from '@rneui/themed';
 
-function Schedule() {
-  return (
-    <>
-    <Text>I AM SCHEDULE</Text>
-    </>
-  );
+class Schedule extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <Text>I know this page sucks... Redo later</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', }}>
+
+        <View style={{flex: 1}} ></View>
+        <View style={{flex: 1}} >
+        <Button
+          onPress={() => this.props.navigation.navigate('List Bartenders')}
+          title={'This is a one-time shift'}
+          containerStyle={{
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+        />
+        <Button
+          title="This is my regular shift"
+          buttonStyle={{ backgroundColor: 'rgba(214, 61, 57, 1)' }}
+          containerStyle={{
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+          titleStyle={{ color: 'white', marginHorizontal: 20 }}
+        />
+        </View>
+        <View style={{flex: 1}} />
+        </View>
+      </>
+    );
+}
 }
 
 export default Schedule;
