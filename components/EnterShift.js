abdd99a077f9e55b3fcb8d399656dc714de50d71
Mark P. Donahue <mark@record360.com>
2023-02-18 17:23:08 -0800

    radio button

--- components/EnterShift.js
+++ components/EnterShift.js
@@ -1,106 +1,199 @@
 import React, { useState } from "react";
-import { Button, View, TextInput, StyleSheet } from "react-native";
+import { Button, View, TextInput, StyleSheet, ImageBackground } from "react-native";
 import { Text } from '@rneui/themed';
 import { CheckBox, Separator } from "react-native-btr";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 import GooglePlacesInput from "./GooglePlacesInput";
+import {Picker} from '@react-native-picker/picker';
 
-const EnterShift = (props) => {
-  // Date Picker
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+import { RadioGroup } from "react-native-btr";
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
+
+class EnterShift extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentForm: 0,
+                  selectedValue: '8',
+                  sameShift: false,
+                  radioButtonsVertical: [
+                    {
+                      id: "1",
+                      label: "Default",
+                    },
+                    {
+                      id: "2",
+                      label: "In-Progress",
+                      selected: true,
+                      color: "#f84",
+                    },
+                    {
+                      id: "3",
+                      label: "Completed",
+                      color: "#484",
+                    }]
+               };
+
+  }
+  handleDateConfirm = (date) => {
+    console.log("A date has been picked: ", date);
+    this.pageForward();
   };
 
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
+  setRadioButtonsVertical = (data) => {
+    console.log(data);
   };
 
-  const handleDateConfirm = (date) => {
-    console.warn("A date has been picked: ", date);
-    hideDatePicker();
+
+  handleTimeConfirm = (time) => {
+    console.log("A Time has been picked: ", time);
+    this.pageForward();
   };
 
-  // Time Picker
-  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
-
-  const showTimePicker = () => {
-    setTimePickerVisibility(true);
-  };
-
-  const hideTimePicker = () => {
-    setTimePickerVisibility(false);
-  };
-
-  const handleTimeConfirm = (Time) => {
-    console.warn("A Time has been picked: ", Time);
-    hideTimePicker();
-  };
-
-  const [notes, onChangeNotes] = React.useState('');
-
-  const [isSelected, setSelection] = useState(false);
-
-  function toggle() {
-    setSelection(!isSelected)
+  toggleSameShift() {
+    const newState = !this.state.sameShift;
+    this.setState({ sameShift: newState })
   }
 
+  selectHours = (hours) => {
+    this.setState({ selectedValue: hours })
+  }
+
+  pageForward = () => {
+    const newState = this.state.currentForm + 1;
+    this.setState({ currentForm: newState })
+  }
+
+  pageBack = () => {
+    const newState = this.state.currentForm - 1;
+    this.setState({ currentForm: newState })
+  }
+
+render(){
+  let form;
+
+  if(this.state.currentForm == 0){
+    form = (
+          <>
+          <Text style={styles.text}>Where are you bartending?</Text>
+            <View style={{backgroundColor: 'white', height: 200, width: "100%", padding: 30}}>
+            <RadioGroup
+              radioButtons={this.state.radioButtonsVertical}
+              onPress={this.setRadioButtonsVertical}
+            />
+            </View>
+          </>
+          )
+
+    // form = (
+    //       <>
+    //       <Text style={styles.text}>Where are you bartending?</Text>
+    //         <View style={{backgroundColor: 'grey', height: 200, width: "100%", padding: 30}}>
+    //           {/* need to pass method as props to this component to pass state up */}
+    //           <GooglePlacesInput />
+    //         </View>
+    //       </>
+    //       )
+    }
+    else if(this.state.currentForm == 1) {
+      form = (
+          <DateTimePickerModal
+            isVisible={this.state.currentForm == 1}
+            mode="date"
+            onConfirm={this.handleDateConfirm}
+            onCancel={this.pageBack}
+          />
+        )
+    }
+    else if(this.state.currentForm == 2) {
+      form = (
+          <DateTimePickerModal
+            isVisible={this.state.currentForm == 2}
+            mode="time"
+            onConfirm={this.handleTimeConfirm}
+            onCancel={this.pageBack}
+          />
+        )
+  }
+    else if(this.state.currentForm == 3) {
+      form = (
+          <>
+          <Text style={styles.text}>How many hours is your shift?</Text>
+          <Picker
+            selectedValue={this.state.selectedValue}
+            style={{ height: 300, width: "100%" }}
+            onValueChange={(itemValue, itemIndex) => this.selectHours(itemValue)}
+          >
+            <Picker.Item label="4" value="4" />
+            <Picker.Item label="5" value="5" />
+            <Picker.Item label="6" value="6" />
+            <Picker.Item label="7" value="7" />
+            <Picker.Item label="8" value="8" />
+            <Picker.Item label="9" value="9" />
+            <Picker.Item label="10" value="10" />
+            <Picker.Item label="11" value="11" />
+            <Picker.Item label="11" value="11" />
+            <Picker.Item label="12" value="12" />
+            <Picker.Item label="13" value="13" />
+          </Picker>
+          </>
+        )
+  }
+//     else if(this.state.currentForm == 4) {
+//       form = (
+//           <>
+//           <Text style={styles.text}>Same shift next week?</Text>
+//             <View style={{backgroundColor: 'grey', height: 200, width: "100%", padding: 30}}>
+//             <Button
+// 
+//           </View>
+//           </>
+//         )
+//   }
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', }}>
-    <View style={{flex: 1}}>
-      <GooglePlacesInput />
-    </View>
-      <View style={{flex: 1}}>
-      <TextInput
-        style={styles.input}
-        numberOfLines={4}
-        onChangeText={onChangeNotes}
-        placeholder="Half off buffalo wings!"
-        value={notes}
-        multiline={true}
-        textAlignVertical='top'
-      />
-      </View>
-      <View style={{flex: 1}}>
-        <Button title="Enter Date" onPress={showDatePicker} />
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={handleDateConfirm}
-          onCancel={hideDatePicker}
-        />
-      </View>
-    <View style={{flex: 1}}>
-      <Button title="Enter Time" onPress={showTimePicker} />
-      <DateTimePickerModal
-        isVisible={isTimePickerVisible}
-        mode="time"
-        onConfirm={handleTimeConfirm}
-        onCancel={hideTimePicker}
-      />
-    </View>
-    <View style={{flex: 1}}>
-    <View style={styles.row}>
-      <CheckBox
-        checked={isSelected}
-        onPress={() => toggle()}
-      />
-      <Text style={styles.label}>Same shift each week?</Text>
-    </View>
-    </View>
-    <View style={{flex: 1}}>
+    <View style={styles.container}>
+      <ImageBackground
+        source={{uri: 'https://cdn.pixabay.com/photo/2014/10/22/17/50/bar-498420_1280.jpg'}}
+        resizeMode="cover"
+        style={styles.image}>
+      <View style={{backgroundColor: 'white', margin: 10, borderRadius: 40, overflow: 'hidden'}}>
+      {form}
       <Button
-        title="Confirm"
-        onPress={() => props.navigation.navigate('Map')}
+        title="Next"
+        style={styles.text}
+        onPress={this.pageForward}
       />
       </View>
+      </ImageBackground>
     </View>
   );
+}
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textInput: {
+    // color: 'white',
+    fontSize: 42,
+    lineHeight: 42,
+    // fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    // color: 'white',
+    fontSize: 24,
+    lineHeight: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'white',
+  },
   input: {
     height: 60,
     margin: 12,
