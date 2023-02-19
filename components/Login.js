2cfb1cf866fb6df1f7724fc11ad06fb5ed32334b
Mark P. Donahue <mark@record360.com>
2023-02-18 19:44:24 -0800

    front-end

--- components/Login.js
+++ components/Login.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, TextInput, Button} from 'react-native';
+import {SafeAreaView,
+        StyleSheet,
+        TextInput,
+        Button,
+        View,
+        Image,
+        ImageBackground} from 'react-native';
+import Bartender from '../assets/bartender2.png'
+import Logo from '../assets/logo.png'
 
 const Login = (props) => {
   const [email, onChangeEmail] = React.useState('');
   const [password, onChangePassword] = React.useState('');
 
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
+      <ImageBackground
+        source={Bartender}
+        resizeMode="cover"
+        style={styles.image}>
+      <Image source={Logo} style={{height: 250, width: 400}} />
       <TextInput
         style={styles.input}
         onChangeText={onChangeEmail}
         placeholder="email"
         value={email}
       />
-
       <TextInput
         style={styles.input}
         onChangeText={onChangePassword}
@@ -21,11 +33,14 @@ const Login = (props) => {
         placeholder="password"
         value={password}
       />
+      <View style={styles.button} >
       <Button
         title="Login"
         onPress={() => props.navigation.navigate('Map')}
       />
-    </SafeAreaView>
+      </View>
+      </ImageBackground>
+    </View>
   );
 };
 
@@ -35,6 +50,19 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    backgroundColor: 'white'
+  },
+  button: {
+    marginHorizontal: 50,
+    marginTop: 20,
+    backgroundColor: 'white'
+  },
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
   },
 });
 
