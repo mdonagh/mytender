9d52a8d70b5168bb98319f5757f4382135d40b96
tonsojun <josephjun@gmail.com>
2023-03-09 10:34:39 -0800

    working signup component and rendering first form

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,20 +1,101 @@
 import React from 'react';
-import {SafeAreaView,
-        StyleSheet,
+import {StyleSheet,
         Text,
         View,
+        ImageBackground,
+        Button
 } from 'react-native';
-import Bartender from '../assets/bartender2.png'
-import Logo from '../assets/logo.png'
 
-const SignUp = () => {
-  return (
-    <View>
-      <Text>Hello from Signup!</Text>
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        currentForm: 0
+      };
+  }
+
+  pageForward = () => {
+    const newState = this.state.currentForm + 1;
+    this.setState({ currentForm: newState})
+  }
+
+  pageBack = () => {
+    const newState = this.state.currentForm - 1;
+    this.setState({ currentform: newState })
+  }
+
+  render () {
+    let form;
+
+    if(this.state.currentForm == 0) {
+      form = (
+        <>
+          <Text>Test this form</Text>
+            <View style={{backGroundColor: 'grey', height: 200, width: "100%", padding: 30}}></View>
+        </>
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+      <ImageBackground
+        source={{uri: 'https://cdn.pixabay.com/photo/2014/10/22/17/50/bar-498420_1280.jpg'}}
+        resizeMode="cover"
+        style={styles.image}>
+      <View style={{backgroundColor: 'white', margin: 10, borderRadius: 40, overflow: 'hidden'}}>
+      {form}
+      <Button
+        title="Next"
+        style={styles.text}
+        onPress={this.pageForward}
+      />
+      </View>
+      </ImageBackground>
     </View>
-  );
-};
+    )
+  }
+}
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textInput: {
+    // color: 'white',
+    fontSize: 42,
+    lineHeight: 42,
+    // fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    // color: 'white',
+    fontSize: 24,
+    lineHeight: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'white',
+  },
+  input: {
+    height: 60,
+    margin: 12,
+    width: 300,
+    borderWidth: 1,
+    padding: 10,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  label: {
+    flex: 1,
+    paddingRight: 150,
+    paddingLeft: 20,
+  },
+});
 
 export default SignUp;
