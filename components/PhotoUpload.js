691c7ea410970ee48d79ee3d54dd3b4cd2770636
Mark P. Donahue <mark@record360.com>
2023-07-01 22:24:32 -0700

    newest

--- components/PhotoUpload.js
+++ components/PhotoUpload.js
@@ -7,18 +7,25 @@ import {
   Image,
 } from 'react-native';
 
-import { Buffer } from 'buffer'
-
-import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
 import md5 from 'md5';
 import { CREATE_PHOTO } from "../gql/createPhoto";
 import { withApollo } from '@apollo/client/react/hoc';
 
 class PhotoUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: false
+    }
+  }
+
   uploadImage = async (imageData, imageExt) => {
     console.log(md5(imageData))
 
   }
+
+  setImage = (params) => this.setState({image: params});
 
   pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -33,7 +40,7 @@ class PhotoUpload extends React.Component {
 
     if (!result.canceled) {
       let uri = result.assets[0].uri
-      setImage(result.assets[0].uri);
+      this.setImage(result.assets[0].uri);
 
       const imagePath = uri;
       const imageExt = uri.split('.').pop();
@@ -55,20 +62,21 @@ class PhotoUpload extends React.Component {
           checksum: checksum,
           contentType: contentType
         },
-      }).catch(error => console.log("An error", error));
+      }).then(result => {
+        console.log(result)
+    }).catch(error => {
+      console.log("An error", error)
+    });
 
-      await uploadImage(imageData, imageExt)
+      await this.uploadImage(imageData, imageExt)
     }
   }
 
 render(){
-  const image = this.state.image;
-  const setImage = (params) => this.setState({image: params});
-
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="Pick an image from camera roll" onPress={this.pickImage} />
+      {this.state.image && <Image source={{ uri: this.state.image }} style={{ width: 200, height: 200 }} />}
     </View>
   );
 }
--- gql/createPhoto.js
+++ gql/createPhoto.js
@@ -1,18 +1,18 @@
 import { gql, useMutation } from '@apollo/client';
 
-export const CREATE_SHIFT = gql`
-mutation EnterShift($filename: String!,
+export const CREATE_PHOTO = gql`
+mutation createPhoto($filename: String!,
                      $byteSize: Int!,
                      $checksum: String!,
                      $contentType: String!) {
-createShift(filename: $filename,
+createPhoto(filename: $filename,
             byteSize: $byteSize,
             checksum: $checksum,
             contentType: $contentType) {
     presigned {
       url
       headers
-      signed_id
+      signedId
     }
   }
 }
