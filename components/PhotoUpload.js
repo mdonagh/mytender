f6bd4e894f23f8b5d9bfa23145369472b01be0dc
Mark P. Donahue <mark@record360.com>
2023-07-01 21:59:37 -0700

    newest

--- components/PhotoUpload.js
+++ components/PhotoUpload.js
@@ -9,9 +9,31 @@ import {
 
 import { Buffer } from 'buffer'
 
+import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
+import md5 from 'md5';
+
 function PhotoUpload() {
   const [image, setImage] = useState(null);
+
+myImageHashFunction = async (imageUri) => {
+  let fsInfo = await FileSystem.readAsStringAsync(imageUri, [{ md5: true }] )
+  console.log(fsInfo)
+}
+
+const getMethods = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+}
+
+  const uploadImage = async (imageData, imageExt) => {
+    console.log(md5(imageData))
+
+  }
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -25,14 +47,25 @@ function PhotoUpload() {
     console.log(result);
 
     if (!result.canceled) {
+      let uri = result.assets[0].uri
       setImage(result.assets[0].uri);
-      const hex = await RNFS.hash(r.path, 'md5')
-      const base64 = Buffer.from(hex, 'hex').toString('base64')
-      console.log(hex)
-      console.log(base64)
-      console.log('meow')
+
+      const imagePath = uri;
+      const imageExt = uri.split('.').pop();
+      const imageMime = `image/${imageExt}`;
+      let picture = await fetch(imagePath);
+      picture = await picture.blob();
+
+      const imageData = new File([picture], `photo.${imageExt}`);
+
+      let byte_size = picture["_data"]["size"]
+      let filename = picture["_data"]["name"]
+      let content_type = picture["_data"]["type"]
+      let checksum = md5(imageData)
+
+      await uploadImage(imageData, imageExt)
     }
-  };
+  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
